7b477accd5448b3e16448fa3a3c0b8957b462331
tabuna <bliz48rus@gmail.com>
2017-09-02 19:48:49 +0300

    update

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+
+
+    <div class="m-t-md m-b-md">
+    <div class="container bg-white b box-shadow m-b-xl">
+    <div class="row">
+
+
+
+    </div>
+    </div>
+    </div>
+
+@endsection
