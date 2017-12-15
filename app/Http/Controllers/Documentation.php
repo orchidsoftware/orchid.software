a00525a5ae80ae3a4d47ad540eea6c5eef643cc5
tabuna <bliz48rus@gmail.com>
2017-12-15 12:01:41 +0300

    cache

--- app/Http/Controllers/Documentation.php
+++ app/Http/Controllers/Documentation.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Storage;
 use Parsedown;
 use Symfony\Component\DomCrawler\Crawler;
@@ -30,7 +31,7 @@ class Documentation
     public function __construct()
     {
         $this->parse = new Parsedown();
-        $this->storage =  Storage::disk('docs');
+        $this->storage = Storage::disk('docs');
         $this->crawler = new Crawler();
     }
 
@@ -39,25 +40,60 @@ class Documentation
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function show($page = 'index'){
+    public function show($page = 'index')
+    {
 
-       $lang = App::getLocale();
+        $lang = App::getLocale();
 
-       $menu = $this->storage->get("/$lang/documentation.md");
-       $contents = $this->storage->get("/$lang/$page.md");
-       $contents = $this->parse->text($contents);
+        $page = $this->getPage($lang, $page);
 
-
-       $this->crawler->addHtmlContent($contents);
-       $title = $this->crawler->filter('h1')->first()->text();
-       $description = $this->crawler->filter('p')->first()->text();
-
-
-       return view('pages.docs',[
-           'menu' => $this->parse->text($menu),
-           'content' => $contents,
-           'title' => $title,
-           'description' => $description,
-       ]);
+        return view('pages.docs', [
+            'menu'        => $this->getMenu($lang),
+            'content'     => $page['content'],
+            'title'       => $page['title'],
+            'description' => $page['description'],
+        ]);
     }
+
+
+    /**
+     * @param $lang
+     *
+     * @return mixed
+     */
+    private function getMenu($lang)
+    {
+
+        return Cache::remember('menu' . $lang, 60, function () use ($lang) {
+            $menu = $this->storage->get("/$lang/documentation.md");
+
+            return $this->parse->text($menu);
+        });
+    }
+
+
+    /**
+     * @param $lang
+     * @param $page
+     *
+     * @return mixed
+     */
+    private function getPage($lang, $page)
+    {
+        return Cache::remember($page . $lang, 60, function () use ($lang, $page) {
+            $contents = $this->storage->get("/$lang/$page.md");
+            $contents = $this->parse->text($contents);
+
+            $this->crawler->addHtmlContent($contents);
+            $title = $this->crawler->filter('h1')->first()->text();
+            $description = $this->crawler->filter('p')->first()->text();
+
+            return [
+                'content'     => $contents,
+                'title'       => $title,
+                'description' => $description,
+            ];
+        });
+    }
+
 }
