c775b1e585e681f07ead235f8db10d149c5cfc8a
Alexandr Chernyaev <bliz48rus@gmail.com>
2019-06-09 22:29:33 +0300

    Update menu-item.blade.php

--- source/_nav/menu-item.blade.php
+++ source/_nav/menu-item.blade.php
@@ -3,11 +3,11 @@
         <h4 class="text-orchid font-thin">{{ $label }}</h4>
 @endif
 
-<li>
+<li class="{{ $page->isActive($url) ? 'active' : '' }}">
     @if ($url = is_string($item) ? $item : $item->url)
         {{-- Menu item with URL--}}
         <a href="{{ $page->url($url) }}"
-            class="{{ 'lvl' . $level }} {{ $page->isActiveParent($item) ? 'lvl' . $level . '-active' : '' }} {{ $page->isActive($url) ? 'active font-semibold text-blue' : '' }} nav-menu__item hover:text-blue"
+            class="{{ 'lvl' . $level }} {{ $page->isActiveParent($item) ? 'lvl' . $level . '-active' : '' }}"
         >
             {{ $label }}
         </a>
