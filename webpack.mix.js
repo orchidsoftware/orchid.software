19cd13277c64a79c6302b3e4aa560e2c4afb9cf7
Alexandr Chernyaev <bliz48rus@gmail.com>
2019-07-24 17:25:13 +0300

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,27 +17,16 @@ mix.webpackConfig({
     ],
 });
 
-/*
-mix.js('source/_assets/js/main.js', 'js')
-    .sourceMaps()
-    .sass('source/_assets/sass/main.scss', 'css/main.css')
-    .options({
-        processCssUrls: false,
-        postCss: [tailwindcss('./tailwind.js')],
-    })
-    .purgeCss({
-        extensions: ['html', 'md', 'js', 'php', 'vue'],
-        folders: ['source'],
-        whitelistPatterns: [/language/, /hljs/, /algolia/],
-    })
-    .version();
-*/
-
 mix
     .sass('source/_assets/sass/app.scss', 'css/app.css', {
         implementation: require('node-sass')
     })
     .copy('./node_modules/orchid-icons/dist/fonts/', 'source/assets/build/fonts')
     .js(['source/_assets/js/app.js'], 'js/app.js')
+    .purgeCss({
+        extensions: ['html', 'md', 'js', 'php', 'vue'],
+        folders: ['source'],
+        whitelistPatterns: [/language/, /hljs/, /algolia/],
+    })
     .sourceMaps()
     .version();
