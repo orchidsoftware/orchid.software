8c8f9c191405b13983d8b05f8242d8ac50c61e02
Alexandr Chernyaev <bliz48rus@gmail.com>
2019-12-23 12:12:43 +0300

    Update ru.blade.php

--- source/_lang/ru.blade.php
+++ source/_lang/ru.blade.php
@@ -9,7 +9,7 @@
     веб-приложения на PHP быстрее, чем когда-либо
 @endsection
 
-@section('main.screen.title','С начало экран')
+@section('main.screen.title','Сначала экран')
 @section('main.screen.description','Создавайте предварительно заполненные поля, добавляйте таблицы и используйте логику для создания надежной страницы.')
 
 @section('main.design.title','Выдержаный стиль')
