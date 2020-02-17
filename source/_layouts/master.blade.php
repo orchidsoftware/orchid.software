64fa307277ef8b53033ad49354332ffea363a3a5
tabuna <bliz48rus@gmail.com>
2020-02-17 21:42:09 +0300

    Https

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="@yield('lang', 'en')">
     <head>
+        <base href="{{ $page->baseUrl }}">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
