2c98551d38546d6ec52bd297b2296141665e8678
tabuna <bliz48rus@gmail.com>
2020-02-17 21:13:59 +0300

    clear

--- source/_assets/js/app.js
+++ source/_assets/js/app.js
@@ -3,17 +3,7 @@ Turbolinks.start();
 
 window.$ = window.jQuery = require('jquery');
 
-window.Popper = require('popper.js').default;
 require('bootstrap');
-
 require('./modules/icons');
 require('./modules/prism');
 require('./modules/docs');
-
-/*
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('./js/worker.js')
-        .then(function() { console.log('Service Worker Registered'); });
-}
-*/
