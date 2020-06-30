4d3ecf7c83b59cfa4fbc9080ced014847aea8f21
Alexandr Chernyaev <bliz48rus@gmail.com>
2020-06-30 19:00:05 +0300

    Update index.blade.php

--- source/_layouts/index.blade.php
+++ source/_layouts/index.blade.php
@@ -11,7 +11,7 @@
                 <div class="col-lg-6">
                     <div class="row pb-5 pt-md-5">
                         <div class="col-md-12">
-                            <h1 class="h2 normal text-white mb-4 mt-4 mr-md-4 text-center text-sm-left font-thin">
+                            <h1 class="text-white mb-4 mt-4 mr-md-4 text-center text-sm-left font-thin display-6">
                                 @yield('main.title')
                             </h1>
                         </div>
