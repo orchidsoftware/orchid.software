9a76aae89561e71ca2b892cb7aeb09b71b6d6b00
tabuna <bliz48rus@gmail.com>
2021-02-05 19:25:27 +0300

    Update whitelistPatterns

--- webpack.mix.js
+++ webpack.mix.js
@@ -29,6 +29,6 @@ if (mix.inProduction()) {
     mix.purgeCss({
         extensions: ['html', 'md', 'js', 'php', 'vue'],
         folders: ['source'],
-        whitelistPatterns: [/language/, /hljs/, /algolia/, /icon/],
+        whitelistPatterns: [/language/, /hljs/, /algolia/, /icon/, /anchor/],
     })
 }
