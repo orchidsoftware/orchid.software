2907b9d12cb478eeb46939dfc95bf8ba554b67f4
tabuna <bliz48rus@gmail.com>
2021-03-20 13:46:16 +0300

    Update recipes

--- source/en/recipes/index.blade.php
+++ source/en/recipes/index.blade.php
@@ -5,7 +5,7 @@
     <div class="bg-white-only">
         <div class="container">
             <div class="row pt-5 pb-5 container">
-                <div class="col-md-8">
+                <div class="col-md-12">
                     <div>
                         <h2 class="text-dark font-thin mt-2 mb-2">
                             <svg class="text-muted mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
@@ -23,7 +23,7 @@
                     </div>
 
                     <div class="row">
-                        <div class="col-12 col-md-6">
+                        <div class="col-12 col-md-4">
 
                             <div class="topics-list">
                                 <h3 class="text-black font-thin">
@@ -78,30 +78,9 @@
                                 </ul>
                             </div>
 
-                            <div class="topics-list">
-                                <h3 class="text-black font-thin">
-
-                                    <svg class="text-muted mr-2" xmlns="http://www.w3.org/2000/svg" width="1em"
-                                         height="1em" fill="currentColor" viewBox="0 0 32 32">
-                                        <path d="M7.11,23.52l1.42-1.41C5.14,18.72,3.37,17,2.39,16L8.53,9.89A1,1,0,1,0,7.11,8.47L.29,15.29A1.05,1.05,0,0,0,0,16a1,1,0,0,0,.3.71Z"></path>
-                                        <path d="M31.71,15.29c-.1-.09-6.82-6.82-6.82-6.82L23.48,9.89c3.38,3.38,5.15,5.16,6.13,6.09l-6.13,6.13a1,1,0,0,0,.7,1.71,1,1,0,0,0,.71-.3l6.82-6.81A1,1,0,0,0,32,16,1,1,0,0,0,31.71,15.29Z"></path>
-                                        <path d="M13.27,27.91a1,1,0,0,0,1-.76L19.7,5.33a1,1,0,0,0-1.94-.48L12.3,26.67A1,1,0,0,0,13,27.88,1,1,0,0,0,13.27,27.91Z"></path>
-
-                                    </svg>
-
-                                    JavaScript
-                                </h3>
-                                <ul class="list-group pl-5">
-                                    <li><a href="/en/recipes/js-call-on-page-refresh">How to call a script whenever the
-                                            page?</a></li>
-                                    <li><a href="#">In the process <i class="text-muted">(Coming soon)</i> </a></li>
-                                    <li><a href="#">In the process <i class="text-muted">(Coming soon)</i> </a></li>
-                                    <li><a href="#">In the process <i class="text-muted">(Coming soon)</i> </a></li>
-                                </ul>
-                            </div>
 
                         </div>
-                        <div class="col-12 col-md-6">
+                        <div class="col-12 col-md-4">
 
                             <div class="topics-list">
                                 <h3 class="text-black font-thin">
@@ -162,6 +141,32 @@
                                 <ul class="list-group pl-5">
                                     <li><a href="#">In the process <i class="text-muted">(Coming soon)</i> </a></li>
 
+                                    <li><a href="#">In the process <i class="text-muted">(Coming soon)</i> </a></li>
+                                    <li><a href="#">In the process <i class="text-muted">(Coming soon)</i> </a></li>
+                                    <li><a href="#">In the process <i class="text-muted">(Coming soon)</i> </a></li>
+                                </ul>
+                            </div>
+
+                        </div>
+
+                        <div class="col-12 col-md-4">
+
+                            <div class="topics-list">
+                                <h3 class="text-black font-thin">
+
+                                    <svg class="text-muted mr-2" xmlns="http://www.w3.org/2000/svg" width="1em"
+                                         height="1em" fill="currentColor" viewBox="0 0 32 32">
+                                        <path d="M7.11,23.52l1.42-1.41C5.14,18.72,3.37,17,2.39,16L8.53,9.89A1,1,0,1,0,7.11,8.47L.29,15.29A1.05,1.05,0,0,0,0,16a1,1,0,0,0,.3.71Z"></path>
+                                        <path d="M31.71,15.29c-.1-.09-6.82-6.82-6.82-6.82L23.48,9.89c3.38,3.38,5.15,5.16,6.13,6.09l-6.13,6.13a1,1,0,0,0,.7,1.71,1,1,0,0,0,.71-.3l6.82-6.81A1,1,0,0,0,32,16,1,1,0,0,0,31.71,15.29Z"></path>
+                                        <path d="M13.27,27.91a1,1,0,0,0,1-.76L19.7,5.33a1,1,0,0,0-1.94-.48L12.3,26.67A1,1,0,0,0,13,27.88,1,1,0,0,0,13.27,27.91Z"></path>
+
+                                    </svg>
+
+                                    JavaScript
+                                </h3>
+                                <ul class="list-group pl-5">
+                                    <li><a href="/en/recipes/js-call-on-page-refresh">How to call a script whenever the
+                                            page?</a></li>
                                     <li><a href="#">In the process <i class="text-muted">(Coming soon)</i> </a></li>
                                     <li><a href="#">In the process <i class="text-muted">(Coming soon)</i> </a></li>
                                     <li><a href="#">In the process <i class="text-muted">(Coming soon)</i> </a></li>
