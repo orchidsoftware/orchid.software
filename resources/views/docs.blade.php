2cf1ccb61f5153a64531a158b291d5b830365b90
tabuna <bliz48rus@gmail.com>
2023-07-30 02:07:05 +0300

    Added experement block "friends"

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -32,8 +32,26 @@
     bottom: 20%">
 
 
-                <div class="col-12 col-md-auto bg-grey py-md-3" style="background:#F9F9FE;z-index: 2">
+                <div class="col-12 col-md-auto bg-grey py-md-3 d-flex flex-column" style="background:#F9F9FE;z-index: 2">
                     <x-docs-menu :section="$section ?? 'site.navigation.'"/>
+
+
+                    <div class="mt-auto pt-5 d-none d-lg-flex flex-column mb-3">
+                        <p class="mb-1 d-none d-md-inline-block text-dark opacity-75 text-center small">
+                        Our Friends
+                    </p>
+
+                        <div class="d-flex flex-column friends">
+                            <a href="https://assisted-mindfulness.com/" class="mt-2 py-3 rounded-3 mx-4" target="_blank" style="background: #2624300f;">
+                                <img src="/img/sponsors/assisted-mindfulness-logo.svg" class="rounded-3" height="40px">
+                            </a>
+
+                            <a href="https://sajya.github.io/" class="mt-2 py-3 rounded-3 mx-4" target="_blank" style="background: #2624300f;">
+                                <img src="/img/sponsors/sajya.svg" class="rounded-3" height="40px">
+                            </a>
+                        </div>
+                    </div>
+
                 </div>
                 <div class="col-12 col-md me-auto bg-white py-md-3">
                     <main class="py-2 px-3 py-md-4 px-md-4 ms-md-4 me-md-auto order-md-first overflow-auto">
