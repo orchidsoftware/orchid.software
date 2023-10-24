81cf16658bef11b45ac9613a98886e33dca9e020
Alexandr Chernyaev <bliz48rus@gmail.com>
2023-10-25 01:39:26 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -433,7 +433,7 @@ class Task extends Screen
                     over the world.
                 </p>
 
-                <a href="/en/license" class="link-secondary d-block stretched-link text-decoration-none link-more px-3">View License
+                <a href="/en/license" class="link-secondary d-block text-decoration-none link-more px-3">View License
                     <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" fill="currentColor" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"></path>
                     </svg>
