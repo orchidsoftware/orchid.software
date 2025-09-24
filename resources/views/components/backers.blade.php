2bebf4da6e8436b97a3fbc8a93034e76adf81745
Alexandr Chernyaev <bliz48rus@gmail.com>
2025-09-24 15:28:03 +0300

    Update backers.blade.php

--- resources/views/components/backers.blade.php
+++ resources/views/components/backers.blade.php
@@ -1,4 +1,4 @@
-<div class="mx-auto text-center text-md-start">
+<div class="mx-auto text-center">
     @foreach($backers as $backer)
         <img src="{{$backer['image'] ?? 'https://avatar.oxro.io/avatar.svg?name='.$backer['name'] }} "
              width="54px" height="54px" class="rounded-circle m-2 d-inline-block shadow border border-dark" loading="lazy">
